1732efaab95fb26424025aaa338bd86cf0f007c1
Javier Chireno <javierchireno04@gmail.com>
2021-07-21 23:41:19 -0400

    commit tree dd4527832e49260c288212a810f1b0547010cd1a

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    console.log(callback());
+}
+
+function returnsANamedFunction() {
+    return function namedFunction() {};
+  }
+
+  function returnsAnAnonymousFunction() {
+    return function () {};
+  }
+
